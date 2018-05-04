0b6606c220941d5b8d94e01406509ac24be18641
Rigo <rigo.galicia@cooperativa-tonantel.com.gt>
2018-05-04 08:11:02 -0600

    Mantenimiento de Roles y Privilegios

---
+++ Cross-selling core/src/java/admin/modelo/Privilegio.java
@@ -0,0 +1,90 @@
+package admin.modelo;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.ArrayList;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class Privilegio {
+    private ObjectId id;
+    private String descripcion;
+    private String forma;
+    private String idRol;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+
+    public String getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(String idRol) {
+        this.idRol = idRol;
+    }
+    
+    /* Metodo para insertar un registro en documento de privilegios */
+    public void insert(){
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("privilegios");
+        Document documento = new Document("descripcion", descripcion)
+                .append("forma", forma)
+                .append("idRol", idRol);
+        coleccion.insertOne(documento);
+    }
+    
+    /* Metodo creado para actualizar un registro del documento de privilegios */
+    public void update(){
+        System.out.println("Se ejecuto el metodo de actualizar");
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("privilegios");
+        coleccion.updateOne(eq("_id", id), 
+                new Document("$set", new Document("descripcion", descripcion)
+                .append("forma", forma)
+                .append("idRol", idRol)));
+    }
+    
+    /* Metodo utilizado para consultar los registro de privilegios */
+    public ArrayList<Privilegio> mostrarPrivilegios(){
+        ArrayList<Privilegio> resultado = new ArrayList<>();
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("privilegios");
+        MongoCursor<Document> cursor = coleccion.find().iterator();
+        
+        try{
+            while(cursor.hasNext()){
+                Document siguiente = cursor.next();
+                Privilegio p = new Privilegio();
+                p.setId(siguiente.getObjectId("_id"));
+                p.setDescripcion(siguiente.getString("descripcion"));
+                p.setForma(siguiente.getString("forma"));
+                p.setIdRol(siguiente.getString("idRol"));
+
+                resultado.add(p);
+            }
+        }finally{
+            cursor.close();
+        }
+        
+        return resultado;
+    }
+}
---
+++ Cross-selling core/src/java/admin/modelo/Rol.java
@@ -0,0 +1,75 @@
+package admin.modelo;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.eq;
+import java.util.ArrayList;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+public class Rol {
+    private ObjectId id;
+    private String descripcion;
+    private String forma;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getForma() {
+        return forma;
+    }
+
+    public void setForma(String forma) {
+        this.forma = forma;
+    }
+    
+    /* Metodo utilizado para insertar in registro de roles */
+    public void insert(){
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("roles");
+        Document documento = new Document("descripcion", this.descripcion)
+                .append("forma", this.forma);
+        coleccion.insertOne(documento);
+    }
+    
+    /* Metodo utilizado para actualizar un registro del documento de roles */
+    public void update(){
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("roles");
+        coleccion.updateOne(eq("_id", this.id), 
+                new Document("$set", new Document("descripcion", this.descripcion)
+                .append("forma", this.forma)));
+    }
+    
+    /* Metodo utilizado para mostrar los registros de roles */
+    public ArrayList<Rol> mostrarRoles(){
+        ArrayList<Rol> resultado = new ArrayList<>();
+        MongoCollection<Document> coleccion = ConexionMongo.getInstance().getDatabase().getCollection("roles");
+        MongoCursor<Document> cursor = coleccion.find().iterator();
+        try{
+            while(cursor.hasNext()){
+                Document siguiente = cursor.next();
+                Rol r = new Rol();
+                r.setId(siguiente.getObjectId("_id"));
+                r.setDescripcion(siguiente.getString("descripcion"));
+                r.setForma(siguiente.getString("forma"));
+                resultado.add(r);
+            }
+        }finally{
+            cursor.close();
+        }
+        return resultado;
+    }
+    
+}
