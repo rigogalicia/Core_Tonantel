9c4879853c831eebabc72efa2663b351cd1338e7
Desarrollo <Desarrollo@190.56.150.166>
2018-09-24 08:04:23 -0600

    Metodo para autorizar

--- Cross-selling core/src/java/av/controlador/AvAutorizarBean.java
+++ Cross-selling core/src/java/av/controlador/AvAutorizarBean.java
@@ -2,10 +2,14 @@
 package av.controlador;
 
 import av.modelo.AutorizarAvaluo;
+import dao.AvSolicitud;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ValueChangeEvent;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 
 @ManagedBean(name = "av_autorizar")
@@ -13,6 +17,12 @@ import javax.faces.event.ValueChangeEvent;
 public class AvAutorizarBean {
     private ArrayList<AutorizarAvaluo> listAutorizar = new ArrayList<>();
     private AutorizarAvaluo autorizar = new AutorizarAvaluo();
+    private char estado;
+    
+        public AvAutorizarBean() {
+        autorizar.setEst('c');
+        listAutorizar = autorizar.consultarAvaluo();
+    }
 
     public ArrayList<AutorizarAvaluo> getListAutorizar() {
         return listAutorizar;
@@ -29,17 +39,54 @@ public class AvAutorizarBean {
     public void setAutorizar(AutorizarAvaluo autorizar) {
         this.autorizar = autorizar;
     }
-    
-    
-    
-        public AvAutorizarBean() {
-            autorizar.setEst('c');
-            listAutorizar = autorizar.consultarAvaluo();
-    }
-        
+     
+    //Meto para hacer el filtro por estado
     public void filtrarDatos(ValueChangeEvent e){
         char newEst[] = e.getNewValue().toString().toCharArray();
         autorizar.setEst(newEst[0]);
         listAutorizar = autorizar.consultarAvaluo();
     }
+    
+    //Metodo para autorizar Estado
+    public void autorizar(){
+        System.out.println("Autorizar ............................");
+        estado = 'd';
+        cambiarEstadoAvaluo();
+    }
+    
+    //Metodo para rechazar avaluo
+    public void rechazar(){
+        System.out.println("Rechar ................................");
+        estado = 'e';
+        cambiarEstadoAvaluo();
+    }
+    
+    //Metodo para autorizar o rechazar avaluo
+    public void cambiarEstadoAvaluo(){
+        
+        EntityManagerFactory emf = null;
+        EntityManager em = null;
+        try {
+            emf = Persistence.createEntityManagerFactory("Cross-selling_corePU");
+            em = emf.createEntityManager();
+            
+            em.getTransaction().begin();
+            
+            AvSolicitud solicitud = em.find(AvSolicitud.class, autorizar.getNumeroSolicitud());
+            solicitud.setEstado(estado);
+            
+            em.getTransaction().commit();
+            
+
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+        finally{
+            if(emf != null && em != null){
+                em.close();
+                emf.close();
+            }
+        }
+    }
+    
 }
