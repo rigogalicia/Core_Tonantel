588644bb8380b401c6acce99bfa974b64278103a
r29galicia <r29galicia@152.150.56.190.static.intelnet.net.gt>
2018-11-29 11:50:14 -0600

    Mapeo de la base de datos.

--- Cross-selling core/src/java/admin/modelo/Main.java
+++ Cross-selling core/src/java/admin/modelo/Main.java
@@ -1,20 +1,9 @@
 package admin.modelo;
 
-import ed.modelo.Interrogante;
-
 public class Main {
     
     public static void main(String args[]){
-        Interrogante i = new Interrogante();
-        i.setConducta("Conductas Genéricas");
-        i.setPuesto("");
-        i.setLider("");
-        
-//        i.insert();
-        
-        for(Interrogante in : i.mostrarInterrogantesFiller()) {
-            System.out.println(in.getDescripcion());
-        }
+        System.out.println("Solo para unificar");
     }
 
 }
